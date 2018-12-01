e90ac27a85182f1c7fbfb48affda440c82080434
guohao <37768861+ScDadaguo@users.noreply.github.com>
2018-12-01 16:27:51 +0800

    终于创建好了

--- src/first.java
+++ src/first.java
@@ -2,6 +2,8 @@ public class first {
     public static void main(String[] args) {
         System.out.println("创建库");
         System.out.println("终于创建好了");
+        System.out.println("333");
+
     }
 
 }
