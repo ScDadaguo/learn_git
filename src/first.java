86499f5c7eba6ce6c2d261c355da47fd799a6d54
guohao <37768861+ScDadaguo@users.noreply.github.com>
2019-05-31 15:06:16 +0800

    learn_git

--- src/first.java
+++ src/first.java
@@ -1,6 +1,6 @@
 public class first {
     public static void main(String[] args) {
-        System.out.println("创建库");
+        System.out.println("创建库master");
         System.out.println("终于创建好");
         System.out.println("333");
         System.out.println("dev");
